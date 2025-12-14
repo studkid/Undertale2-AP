388b56513a4cbacbd9e7e77ddc73c038a4a5d374
Elly <eric.poore@gmail.com>
2025-12-14 05:40:29 -0600

    Finally fix the DataManager class being accidentally renamed

--- Game/Ruby/gems/archipelago_rb/archipelago_rb/data.rb
+++ Game/Ruby/gems/archipelago_rb/archipelago_rb/data.rb
@@ -5,7 +5,7 @@ module Archipelago
 
     class Client
 
-        class Data
+        class DataManager
             attr_reader :game_data, :datapackages
 
             # The DataManager handles data about the current game state. It also imports DataPackages from your local filesystem (or writes should they not exist.)
